ad9b11cd2cb576ee46fb87321d318984a4b2635c
Gohost <Joshua32eb@gmail.com>
2026-01-10 12:39:56 -0500

    feat(artboard): add font preloading and Next.js font support

--- app/artboard/[id]/layout.tsx
+++ app/artboard/[id]/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
+import { getAllNextFontClassNames } from "@/lib/fonts/next-fonts";
 
 export const metadata: Metadata = {
   title: "Resume Artboard",
 };
+
+// Get all next/font class names for the layout
+const fontClassNames = getAllNextFontClassNames();
 
 export default function ArtboardLayout({
   children,
@@ -10,8 +14,16 @@ export default function ArtboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={fontClassNames}>
       <head>
+        {/* Preconnect for faster Google Fonts loading (for non-popular fonts) */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        {/* Phosphor icons */}
         <link
           rel="stylesheet"
           href="https://unpkg.com/@phosphor-icons/web@2.0.3/src/bold/style.css"

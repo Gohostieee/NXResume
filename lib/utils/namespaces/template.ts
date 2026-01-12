411e35cdf310b15b2fca123aaaac64c86a1bf531
Gohost <Joshua32eb@gmail.com>
2026-01-11 23:35:27 -0500

    chore: limit templates to Harvard only for QC migration

--- lib/utils/namespaces/template.ts
+++ lib/utils/namespaces/template.ts
@@ -1,17 +1,5 @@
 export const templatesList = [
-  "azurill",
-  "bronzor",
-  "chikorita",
-  "ditto",
-  "gengar",
-  "glalie",
   "harvard",
-  "kakuna",
-  "leafish",
-  "nosepass",
-  "onyx",
-  "pikachu",
-  "rhyhorn",
 ] as const;
 
 export type Template = (typeof templatesList)[number];
